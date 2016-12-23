dd91a77a81136d0c4b0b8d57b814c8b707efd8dd
Lukasz Jakubek <lukasz.jakubek@aplisys.pl>
2016-12-24 00:59:48 +0100

    Add php-xdebug and update nginx config

--- provision/setup.sh
+++ provision/setup.sh
@@ -33,7 +33,7 @@ echo "== Instaling PHP"
 apt-get -y install php7.0 php7.0-fpm php7.0-common php7.0-cli
 
 echo "== Installing PHP extensions"
-apt-get -y install php7.0-apcu php7.0-bcmath php7.0-bz2 php7.0-curl php7.0-gd php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring php7.0-mysql php7.0-opcache php7.0-pgsql php7.0-readline php7.0-sqlite3 php7.0-xml php7.0-xmlrpc php7.0-zip php-pear
+apt-get -y install php7.0-apcu php7.0-bcmath php7.0-bz2 php7.0-curl php7.0-gd php7.0-intl php7.0-json php7.0-ldap php7.0-mbstring php7.0-mysql php7.0-opcache php7.0-pgsql php7.0-readline php7.0-sqlite3 php7.0-xdebug php7.0-xml php7.0-xmlrpc php7.0-zip php-pear
 
 echo "== Installing MariaDB"
 
